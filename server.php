b6bf0dcedfc3cf8c84e7c7a21e1cfe6544847020
opentech4u@gmail.com <opentech4u@gmail.com>
2020-02-05 14:13:53 +0530

    add files

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI']
+
+// 
